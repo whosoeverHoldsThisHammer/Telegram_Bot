e423531348f64546b988db82623f20554406587b
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-14 10:29:12 -0300

    Se agrega lógica para distinguir mensajes tipo callback

--- controllers/messages.js
+++ controllers/messages.js
@@ -33,8 +33,8 @@ const sendMessageWithButton = (chatId, message) => {
       reply_markup: {
         'inline_keyboard': [
             [
-                { "text": "👍🏻 Me sirve", "callback_data": "positive" },
-                { "text": "👎🏻 No me sirve", "callback_data": "negative" }
+                { "text": "👍🏻", "callback_data": "positive" },
+                { "text": "👎🏻", "callback_data": "negative" }
             ]
         ]
       }
@@ -48,10 +48,68 @@ const handleMessage = async(req, res) => {
     try {
         //console.log(req.body)
 
+        let chatId
+
         if (req.body.callback_query) {
-            console.log('Callback query');
+            // console.log('Callback query');
+            // console.log(req.body.callback_query.message.chat)
+
+            const { message } = req.body.callback_query
+            let chatId = message.chat.id
+
+            console.log(req.body.callback_query.data)
+
+            let answer = "Gracias por el feedback"
+
+            sendMessage(chatId, answer)
+            .then(result => console.log("Mensaje enviado"))
+            .catch(error => console.log("Algo salío mal"))
+
         } else {
-            console.log('Mensajer normal');
+            const { message } = req.body
+
+            chatId = message.chat.id
+            let answer
+
+            if(message.photo){
+            
+                answer = "Lo siento. No estoy preparado para interpretar imágenes.\n Por favor, cargá un ticket en Jira."
+    
+            } else if(message.voice){
+                
+                answer = "Lo siento. No estoy preparado para interpretar audios.\n Por favor, cargá un ticket en Jira."
+    
+            } else if(message.document){
+                
+                answer = "Lo siento. No estoy preparado para interpretar documentos.\n Por favor, cargá un ticket en Jira."
+    
+            } else if (message.poll){
+                
+                answer = "Lo siento. No estoy preparado para responder encuestas.\n ¿Qué querías preguntarme?."
+    
+            } else {
+    
+                // answer = "Hola, en qué puedo ayudarte?" // Reemplazar por llamada a servicio integrador o LLM
+                // answer = "Todavía no puedo contestarte preguntas de la base de conocimiento \n\n [Te mando un pikachu](https://www.destructoid.com/wp-content/uploads/2020/12/473652-pika.jpg)"
+    
+                if(isStartCommand(message.text)){
+                    answer = "Bienvenido!"
+
+                    sendMessage(chatId, answer)
+                    .then(result => console.log("Mensaje enviado"))
+                    .catch(error => console.log("Algo salío mal"))
+
+                } else {
+                    answer = "Respuesta generada por IA"
+
+                    sendMessageWithButton(chatId, answer)
+                    .then(result => console.log("Mensaje enviado"))
+                    .catch(error => console.log("Algo salío mal"))
+
+                }  
+    
+            }
+
         }
 
         // console.log(req.body.callback_query)
