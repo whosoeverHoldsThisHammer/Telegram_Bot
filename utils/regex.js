1f95115bd6d86788d0e9fc410e969aa758b9d7ac
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-13 22:24:40 -0300

    Se agrega validación para detectar emojis

---
+++ utils/regex.js
@@ -0,0 +1,7 @@
+const pattern = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+
+const isEmoji = (message) => {
+    return pattern.test(message);
+}
+
+export { isEmoji }
